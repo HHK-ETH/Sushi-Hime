bc4fa6c9a5f17b50fbe597f0c8ac354b1b933746
HHK <hhk.contact@protonmail.com>
2021-11-14 19:12:02 +0100

    feat: add tests (wip)

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,48 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+let owner: SignerWithAddress;
+let user: SignerWithAddress;
+let linkToken: Contract;
+let sushiHime: Contract;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("SushiHime", function () {
+  before("Deploy and setup contracts", async function () {
+    [owner, user] = await ethers.getSigners();
+    const LinkToken = await ethers.getContractFactory("ERC20Mock");
+    linkToken = await LinkToken.connect(owner).deploy(
+      "LINK",
+      "LINK",
+      BigNumber.from(1000).mul(BigNumber.from(10).pow(18))
+    );
+    await linkToken.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    const SushiHime = await ethers.getContractFactory("SushiHime");
+    sushiHime = await SushiHime.deploy(
+      linkToken.address,
+      owner.address,
+      "https://tokenuri.com/",
+      [0, 10000]
+    );
+    await sushiHime.deployed();
+    linkToken.transfer(sushiHime.address, await linkToken.totalSupply());
   });
+
+  it("Should not mint if not owner", async function () {
+    await expect(
+      sushiHime.connect(user).mint(user.address)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+    await expect(
+      sushiHime.connect(user).mintMultiple([user.address])
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("Should not mint if no random", async function () {});
+  it("Should not mint if max supply reached", async function () {});
+  it("Should mint one nft", async function () {});
+  it("Should mint multiple nft", async function () {});
+  it("Should update token uri", async function () {});
+  it("Should not updated token uri if not owner", async function () {});
 });
